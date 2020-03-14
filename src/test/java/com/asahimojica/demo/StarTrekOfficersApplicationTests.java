567cd8b5e276a5a46d867f679b34b883f6f899cb
Asahi <asahimojica@yahoo.com>
2020-03-14 11:48:21 -0500

    did it work?

---
+++ src/test/java/com/asahimojica/demo/StarTrekOfficersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asahimojica.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarTrekOfficersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
